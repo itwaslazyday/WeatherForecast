b25fe6dfd9d45594be93fbecbcd6bb0dd61d256d
Вячеслав <pudra@live.ru>
2022-08-28 23:10:10 +0300

    Получает координаты из поля поиска

--- project/src/components/search-control/search-control.tsx
+++ project/src/components/search-control/search-control.tsx
@@ -12,14 +12,34 @@ function SearchControl () {
       autoClose: true,
       showMarker: true,
       animateZoom: true,
-      retainZoomLevel: true
+      retainZoomLevel: true,
+      maxSuggestions: 8,
+      searchLabel: 'Введите город...',
+      classNames: {
+        container: 'leaflet-bar leaflet-control leaflet-control-geosearch',
+        button: 'leaflet-bar-part leaflet-bar-part-single',
+        resetButton: 'reset reset--styled',
+        msgbox: 'leaflet-bar message',
+        form: '',
+        input: '',
+        resultlist: '',
+        item: '',
+        notfound: 'leaflet-bar-notfound',
+      }
     });
+
     map.addControl(searchControl);
 
     return () => {
       map.removeControl(searchControl);
     };
   }, [map]);
+
+  const searchEventHandler = (result: any) => {
+    console.log(result.location);
+  };
+
+  map.on('geosearch/showlocation', searchEventHandler);
 
   return null;
 }
