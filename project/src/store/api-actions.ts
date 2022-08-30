54b3c80d7a04cb12a3510498c719ae204d3fde9f
Вячеслав <pudra@live.ru>
2022-08-30 22:43:49 +0300

    Сохраняет начало наработок

---
+++ project/src/store/api-actions.ts
@@ -0,0 +1,17 @@
+import {AxiosInstance} from 'axios';
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import {AppDispatch, State} from '../types/state.js';
+import {Forecast, Coord} from '../types/types.js';
+import { API_KEY } from 'services/api.js';
+
+export const fetchCityAction = createAsyncThunk<Forecast, Coord, {
+  dispatch: AppDispatch,
+  state: State,
+  extra: AxiosInstance
+}>(
+  'data/city',
+  async ({lat, lon}, {dispatch, extra: api}) => {
+    const {data} = await api.get<Forecast>(`forecast?lat=${lat}6&lon=${lon}&appid=${API_KEY}`);
+    return data;
+  },
+);
